20ad2f863d2355aada9dedfc341b7184ebce5858
luo3300612 <591486669@qq.com>
2018-11-26 00:10:35 +0800

    update

--- code/fluentpy/chapter21/record_factory.py
+++ code/fluentpy/chapter21/record_factory.py
@@ -2,3 +2,33 @@
 除非开发框架或寻找乐趣，否则不要使用元类
 """
 
+
+def record_factory(cls_name, field_names):
+    try:
+        field_names = field_names.replace(',', ' ').split()
+    except AttributeError:
+        pass
+
+    field_names = tuple(field_names)
+
+    def __init__(self, *args, **kwargs):
+        attrs = dict(zip(self.__slots__, args))
+        attrs.update(kwargs)
+        for name, value in attrs.items():
+            setattr(self, name, value)
+
+    def __iter__(self):
+        for name in self.__slots__:
+            yield getattr(self, name)
+
+    def __repr__(self):
+        values = ', '.join('{}={!r}'.format(*i) for i in zip(self.__slots__, self))
+        return '{}({})'.format(self.__class__.__name__, values)
+
+    cls_attrs = dict(
+        __slots__=field_names,
+        __init__=__init__,
+        __iter__=__iter__,
+        __repr__=__repr__,
+    )
+    return type(cls_name, (object,), cls_attrs) # type 当类使用时，传入三个参数可以新建一个类
